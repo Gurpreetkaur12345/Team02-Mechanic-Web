66b356530aa6d7ef76c4fce73caa188df0e3bc40
Gurpreetkaur12345 <59642896+Gurpreetkaur12345@users.noreply.github.com>
2020-01-16 20:14:16 -0800

    Add files via upload

---
+++ gurpreetplugin/view/new-page.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="<?php echo PLUGIN_URL.'/gurpreetplugin/assets/css/style.css' ?>"/>
+<script> src="<?php echo PLUGIN_URL.'/gurpreetplugin/assets/js/main.js' ?>" </script>
+
+
+
+<?php
+echo "Welcome to New Page";
+
+?>
+
+<h4> Beautiful Image </h4>
+<p>
+<img src="<?php echo PLUGIN_URL.'/gurpreetplugin/assets/images/1.jpg' ?>" style="height:200px;width:200px"/>
+</P>
+
+echo '<div class="container">
+  <div id="slides">
+    <img src="<?php echo PLUGIN_URL.'/gurpreetplugin/assets/images/1.jpg' ?>" />
+    <img src="<?php echo PLUGIN_URL.'/gurpreetplugin/assets/images/1.jpg' ?>" />
+    <img src="<?php echo PLUGIN_URL.'/gurpreetplugin/assets/images/1.jpg' ?>"/>
+    <img src=<?php echo PLUGIN_URL.'/gurpreetplugin/assets/images/1.jpg' ?>" />
+    <a href="#" class="slidesjs-previous slidesjs-navigation"><i class="icon-chevron-left icon-large"></i></a>
+    <a href="#" class="slidesjs-next slidesjs-navigation"><i class="icon-chevron-right icon-large"></i></a>
+  </div>
+</div>';
+}
