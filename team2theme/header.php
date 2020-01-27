4f69cf6228a35eec8e5aa158a8dc9162536cd9b0
Gurpreetkaur12345 <59642896+Gurpreetkaur12345@users.noreply.github.com>
2020-01-26 21:46:08 -0800

    Add files via upload
    Made changes in header and index

--- team2theme/header.php
+++ team2theme/header.php
@@ -15,7 +15,7 @@
       <div id="logo">
         <div id="logo_text">
           <!-- class="logo_colour", allows you to change the colour of the text -->
-          <h1><a href="index.html">Kelvin Grove Tyres and <span class="logo_colour">Mechanical</span></a></h1>
+          <h1><a href="index.html">Kelvin Grove Tyres and Mechanical</span></a></h1>
           <h2>Simple. Contemporary. Website Template.</h2>
         </div>
       </div>
