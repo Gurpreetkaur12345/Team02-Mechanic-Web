0693dc4fdf437ef2ff7f50c01c649e5fd4dacc85
shashanktrip <59642319+shashanktrip@users.noreply.github.com>
2020-02-02 12:40:27 +1000

    Update about.php

--- team2theme/about.php
+++ team2theme/about.php
@@ -6,4 +6,16 @@
 <?php get_header(); ?>
 	
 	
-	
+<div class="ct-pageWrapper" id="ct-js-wrapper">
+  <section class="company-heading intro-type" id="parallax-one">
+    <div class="mechanic">
+      <div class="row product-title-info">
+        <div class="col-md-12">
+          <h1>About Us</h1>
+        </div>
+      </div>
+    </div>
+    <div class="parallax" id="parallax-cta" style="background;"></div>
+  </section>
+  <section class="story-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
+    <div class="mechanic text-center">
